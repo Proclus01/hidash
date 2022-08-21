9f42ab857a2ca6f6664838fdb379d21360e43096
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-21 16:38:11 -0700

    add comments to explain how the testing works

--- index.test.js
+++ index.test.js
@@ -1,12 +1,16 @@
 import hidash from './index.js';
 
 // A simple example of Manual Testing of code
+
+// Initialize a counter to collect values from our example function
 let sum = 0;
 
+// Run the function and pass in a callback
 hidash.forEach([1, 2, 3], value => {
     sum += value;
 });
 
+// After the function is run, check counter against a conditional (this is our test condition)
 if (sum !== 6) {
     throw new Error('Expected summing array to equal to 6');
 }
