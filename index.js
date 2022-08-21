00a856fe37700a3e95d4791525de2f7fa66b56fe
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-21 16:30:49 -0700

    refactor to include index test file, add module type declaration to package.json

--- index.js
+++ index.js
@@ -11,9 +11,4 @@ const hidash = {
     }
 };
 
-// A simple example of Manual Testing of code
-hidash.forEach([1, 2, 3], value => {
-    console.log(value);
-});
-
 export default hidash;
---
+++ index.test.js
@@ -0,0 +1,6 @@
+import hidash from './index.js';
+
+// A simple example of Manual Testing of code
+hidash.forEach([1, 2, 3], value => {
+    console.log(value);
+});
