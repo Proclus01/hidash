16dc8c94466806ed518d13348000f938417c6135
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-21 16:33:55 -0700

    create a valid testing example

--- index.test.js
+++ index.test.js
@@ -1,6 +1,12 @@
 import hidash from './index.js';
 
 // A simple example of Manual Testing of code
+let sum = 0;
+
 hidash.forEach([1, 2, 3], value => {
-    console.log(value);
+    sum += value;
 });
+
+if (sum !== 6) {
+    throw new Error('Expected summing array to equal to 6');
+}
