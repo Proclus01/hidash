464fdcfaec3eee09eb8a6360348f0c5c165e5591
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-21 16:26:37 -0700

    create object to store function definitions, perform manual test of function

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-//jshint esversion:6
-const express = require("express");
+const hidash = {
+    // An implementation of forEach from scratch
+    forEach(array, fn) {
+        for (let i = 0; i < array.length; i++) {
+            // Store array value at index
+            const value = array[i];
 
-// Add something in here
+            // Call function and pass in value and index
+            fn(value, i);
+        }
+    }
+};
 
+// A simple example of Manual Testing of code
+hidash.forEach([1, 2, 3], value => {
+    console.log(value);
+});
+
+export default hidash;
