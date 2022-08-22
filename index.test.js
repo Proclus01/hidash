c886048abd8e07f1cfe5f10a6caf8f8054d9968f
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-21 17:27:46 -0700

    refactor tests with a test helper function to solve common problem cases with test file

--- index.test.js
+++ index.test.js
@@ -1,38 +1,53 @@
-import hidash from './index.js';
+import hidash from "./index.js";
+
+// Test helper function 
+// -- rescopes any global variables to within the test function
+const test = (description, fn) => {
+    // -- automated descriptions for every test run as a title, right above any error message
+    console.log("----", description);
+
+    // -- ensures that the program never crashes when the test fails
+    try {
+        fn();
+    } catch (err) {
+        console.log(err.message);
+    }
+};
 
 // 1. A simple example of Manual Testing of code
 
-// Initialize a counter to collect values from our example function
-let sum = 0;
+test("Test the forEach function", () => {
+    // Initialize a counter to collect values from our example function
+    let sum = 0;
 
-// Run the function and pass in a callback
-hidash.forEach([1, 2, 3], value => {
-    sum += value;
+    // Run the function and pass in a callback
+    hidash.forEach([1, 2, 3], (value) => {
+        sum += value;
+    });
+
+    // After the function is run, check counter against a conditional (this is our test condition)
+    if (sum !== 6) {
+        throw new Error("Expected summing array to equal to 6");
+    }
 });
-
-// After the function is run, check counter against a conditional (this is our test condition)
-if (sum !== 6) {
-    throw new Error('Expected summing array to equal to 6');
-}
-
-// 
-
-// 
 
 // 2. An example of test driven development where we first create our tests before implementing a function
 
-// Run the function first
-const result = hidash.map([1, 2, 3], value => {
-    return value * 2;
+test("Test the map function", () => {
+    // Run the function first
+    const result = hidash.map([1, 2, 3], (value) => {
+        return value * 2;
+    });
+
+    // Use a chain of conditionals to check every array value
+    if (result[0] !== 2) {
+        throw new Error(`Expected to find 2, but found ${result[0]}`);
+    }
+    if (result[1] !== 4) {
+        throw new Error(`Expected to find 4, but found ${result[1]}`);
+    }
+    if (result[2] !== 6) {
+        throw new Error(`Expected to find 6, but found ${result[2]}`);
+    }
 });
 
-// Use a chain of conditionals to check every array value
-if (result[0] !== 2) {
-    throw new Error(`Expected to find 2, but found ${result[0]}`);
-} 
-if (result[1] !== 4) {
-    throw new Error(`Expected to find 4, but found ${result[1]}`);
-}
-if (result[2] !== 6) {
-    throw new Error(`Expected to find 6, but found ${result[2]}`);
-}
